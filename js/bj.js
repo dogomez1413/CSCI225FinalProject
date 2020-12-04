239641590024da4a1f5a3b8dd965b1cb0af7e484
AMKimsey2000 <amkimsey@usca.edu>
2020-12-03 19:25:04 -0500

    Added basic gameplay

--- js/bj.js
+++ js/bj.js
@@ -34,7 +34,7 @@ function decodeCard(cardNum) {
     }
     return [(cardNum % 13) + 1, Math.floor(cardNum / 13), stringNum + " of " + suits[Math.floor(cardNum / 13)]];
 }
-function drawCard(cardNum, hidden) {
+function genCard(cardNum, plr, hidden) {
     var cardVals = decodeCard(cardNum);
     var suitSymbols = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
     var numLetter;
@@ -55,14 +55,86 @@ function drawCard(cardNum, hidden) {
             numLetter = String(cardVals[0]);
             break;
     }
+    var player = (plr ? "#plrCardArea" : "#cpuCardArea")
     var id = (hidden ? "card-hidden" : "card") + (cardVals[1] < 2 ? "" : "-red");
-    $("#plrCardArea").append("<div id = '" + id + "'><span id = 'numL'>" + numLetter + "</span><span id = 'suit'>" + suitSymbols[cardVals[1]] + "</span><span id = 'numR'>" + numLetter + "</span></div>");
+    $(player).append("<div id = '" + id + "' value = '" + cardVals[0] + "'><span id = 'numL'>" + numLetter + "</span><span id = 'suit'>" + suitSymbols[cardVals[1]] + "</span><span id = 'numR'>" + numLetter + "</span></div>");
+    //console.log((plr ? "Player" : "Computer") + " drew a " + cardVals[2] + "\nCard value: " + cardVals[0] + "\nSuit index number: " + cardVals[1] + " = " + suitSymbols[cardVals[1]] + "\nCard hidden: " + hidden);
 }
-drawCard(0, false);
-drawCard(13, false);
-drawCard(26, false);
-drawCard(39, false);
-drawCard(21, false);
-drawCard(36, false);
-drawCard(44, true)
-drawCard(9, true)
+function game() {
+    var cards = generateRandomCards();
+    var currIndex = 0;
+    var plrScore = 0;
+    var cpuScore = 0;
+    var plrBust = false;
+    var cpuBust = false;
+    for (var i = 0; i < 4; i++) {
+        genCard(cards[currIndex], i < 2, i == 3);
+        currIndex++;
+    }
+    updateScore();
+    function updateScore() {
+        plrScore = cpuScore = 0;
+        $("#plrCardArea div").each(function() {
+            var val = parseInt($(this).attr("value"));
+            plrScore += (val > 10 ? 10 : val);
+        });
+        $("#cpuCardArea div").each(function() {
+            var val = parseInt($(this).attr("value"));
+            cpuScore += (val > 10 ? 10 : val);
+        });
+        //console.log("Updated scores\nPlr: " + plrScore + "\nComp: " + cpuScore);
+        return [plrScore, cpuScore];
+    }
+    function hit(plr) {
+        genCard(cards[currIndex], plr, false);
+        currIndex++;
+        var scores = updateScore();
+        if (plr && scores[0] > 21) {
+            plrBust = true;
+            $("#hit").css("visibility", "hidden");
+            $("#stand").css("visibility", "hidden");
+            cpu();
+        }
+    }
+    function cpu() {
+        var currScore = updateScore()[1];
+        while (currScore < 17) {
+            hit(false);
+            currScore = updateScore()[1];
+        }
+        cpuBust = currScore > 21
+        $("#cpuCardArea div[id *= 'hidden']").attr("id", String($("#cpuCardArea div[id *= 'hidden']").attr("id")).replace("-hidden", ""));
+        gameEnd();
+    }
+    function gameEnd() {
+        var scores = updateScore();
+        if (plrBust || cpuBust) {
+            if (plrBust && cpuBust) {
+                $("#winner").text("Tie");
+            } else if (plrBust) {
+                $("#winner").text("You lose!");
+            } else {
+                $("#winner").text("You're winner!");
+            }
+        } else {
+            if (scores[0] > scores[1]) {
+                $("#winner").text("You're winner!");
+            } else if (scores[0] < scores[1]) {
+                $("#winner").text("You lose!");
+            } else {
+                $("#winner").text("Tie");
+            }
+        }
+        $("#hit").off("click");
+        $("#stand").off("click");
+    }
+    $("#hit").click(function() {
+        hit(true);
+    });
+    $("#stand").click(function() {
+        $("#hit").css("visibility", "hidden");
+        $("#stand").css("visibility", "hidden");
+        cpu();
+    });
+}
+game()
